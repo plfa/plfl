426f9bc2b2f83cc56136876ae8e8affff99e7a85
wadler <wadler@inf.ed.ac.uk>
2022-06-28 19:45:13 -0300

    Swap parentheses in statement of add_assoc

--- src/part1/Induction.lean
+++ src/part1/Induction.lean
@@ -2,22 +2,30 @@ open Nat
 
 namespace Induction
 
+example : 3 + (4 + 5) = (3 + 4) + 5 :=
+  calc
+    3 + (4 + 5)
+      =  3 + 9        := rfl
+    _ =  12           := rfl
+    _ =  7 + 5        := rfl
+    _ =  (3 + 4) + 5  := rfl
+
 theorem add_zero (m : Nat)   : m + zero     = m            := rfl
 theorem add_succ (m n : Nat) : m + (succ n) = succ (m + n) := rfl
 
-theorem add_assoc : ∀ (m n p : Nat), (m + n) + p = m + (n + p)
+theorem add_assoc : ∀ (m n p : Nat), m + (n + p) = (m + n) + p
   | m , n , zero =>
     calc
-      (m + n) + zero
-        = m + n               := by rw [add_zero (m + n)]
-      _ = m + (n + zero)      := by rw [add_zero n]
+      m + (n + zero)
+        = m + n               := by rw [add_zero n]
+      _ = (m + n) + zero      := by rw [add_zero (m + n)]
   | m , n , succ p =>
     calc
-      (m + n) + succ p
-        = succ ((m + n) + p)  := by rw [add_succ (m + n) p]
-      _ = succ (m + (n + p))  := by rw [add_assoc m n p]
-      _ = m + succ (n + p)    := by rw [add_succ m (n + p)]
-      _ = m + (n + succ p)    := by rw [add_succ n p]
+      m + (n + succ p)
+        = m + succ (n + p)    := by rw [add_succ n p]
+      _ = succ (m + (n + p))  := by rw [add_succ m (n + p)]
+      _ = succ ((m + n) + p)  := by rw [add_assoc m n p]
+      _ = (m + n) + succ p    := by rw [add_succ (m + n) p]
 
 theorem zero_add : ∀ (n : Nat), zero + n = n
   | zero =>
