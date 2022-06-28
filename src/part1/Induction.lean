5f55c7fe9bd8e719ec58501ea10befda98bb8657
wadler <wadler@inf.ed.ac.uk>
2022-06-28 18:29:35 -0300

    first draft of Induction

--- src/part1/Induction.lean
+++ src/part1/Induction.lean
@@ -1,4 +1,22 @@
+open Nat
+
 namespace Induction
 
-theorem add_assoc (m n p : Nat) : (m + n) + p = m + (n + p) :=
-  by induction p with p where
+theorem add_zero (m : Nat)   : m + zero     = m            := rfl
+theorem add_succ (m n : Nat) : m + (succ n) = succ (m + n) := rfl
+
+theorem zero_add : ∀ (n : Nat), zero + n = n
+  | zero   =>
+    calc
+      zero + zero
+        = zero              := rfl
+  | succ n => 
+    calc
+      zero + succ n
+        =  succ (zero + n)  := rfl
+      _ =  succ n           := by rw [zero_add n]
+
+  
+  
+  -- congrArg succ (zero_add n)
+
