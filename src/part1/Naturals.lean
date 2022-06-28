3815337110d5f174655c328095d9aa6468215389
wadler <wadler@inf.ed.ac.uk>
2022-06-28 19:28:52 -0300

    revised Naturals to use Nat and _root_

--- src/part1/Naturals.lean
+++ src/part1/Naturals.lean
@@ -1,73 +1,72 @@
-
 namespace Naturals
 
-inductive ℕ where
-  | zero : ℕ
-  | succ : ℕ → ℕ
+inductive Nat where
+  | zero : Nat
+  | succ : Nat → Nat
 
-open ℕ
+open Nat
 
-def ℕ_of_Nat : Nat → ℕ 
-  | Nat.zero    =>  zero
-  | Nat.succ n  =>  succ (ℕ_of_Nat n)
+def convert : _root_.Nat → Nat 
+  | _root_.Nat.zero    =>  zero
+  | _root_.Nat.succ n  =>  succ (convert n)
 
-instance : OfNat ℕ n where
-  ofNat := ℕ_of_Nat n
+instance : OfNat Nat n where
+  ofNat := convert n
 
-def add : ℕ → ℕ → ℕ
+def add : Nat → Nat → Nat
   | m , zero   => m
   | m , succ n => succ (add m n)
 
-instance : Add ℕ where
+instance : Add Nat where
   add := add
 
-example : 3 + 2 = (5 : ℕ) :=
+example : 3 + 2 = (5 : Nat) :=
   calc
     3 + 2 
       =  succ (3 + 1)         := rfl
     _ =  succ (succ (3 + 0))  := rfl
     _ =  succ (succ 3)        := rfl
-    _ =  (5 : ℕ)              := rfl
+    _ =  (5 : Nat)            := rfl
 
 -- a shorter proof
 example : 3 + 2 = 5 := rfl
 
-def mul : ℕ → ℕ → ℕ
+def mul : Nat → Nat → Nat
   | _ , zero   => zero
   | m , succ n => (mul m n) + m
 
-instance : Mul ℕ where
+instance : Mul Nat where
   mul := mul
 
-example : 3 * 2 = (6 : ℕ) :=
+example : 3 * 2 = (6 : Nat) :=
   calc
     3 * 2  
-      =  (3 * 1 + 3 : ℕ)     := rfl
-    _ =  (3 * 0 + 3 + 3 : ℕ) := rfl
-    _ =  (0 + 3 + 3 : ℕ)     := rfl
-    _ =  (6 : ℕ)             := rfl
+      =  (3 * 1 + 3 : Nat)     := rfl
+    _ =  (3 * 0 + 3 + 3 : Nat) := rfl
+    _ =  (0 + 3 + 3 : Nat)     := rfl
+    _ =  (6 : Nat)             := rfl
 
-def monus : ℕ → ℕ → ℕ
+def monus : Nat → Nat → Nat
   | m      , zero    =>  m
   | zero   , succ _  =>  zero
   | succ m , succ n  =>  monus m n
 
-instance : Sub ℕ where
+instance : Sub Nat where
   sub := monus
 
-example : 3 - 2 = (1 : ℕ) :=
+example : 3 - 2 = (1 : Nat) :=
   calc
     3 - 2  
-      =  (2 - 1 : ℕ)  := rfl
-    _ =  (1 - 0 : ℕ)  := rfl
-    _ =  (1 : ℕ)     := rfl
+      =  (2 - 1 : Nat)  := rfl
+    _ =  (1 - 0 : Nat)  := rfl
+    _ =  (1 : Nat)      := rfl
 
-example : 2 - 3 = (0 : ℕ) :=
+example : 2 - 3 = (0 : Nat) :=
   calc
     2 - 3  
-      =  (1 - 2 : ℕ)  := rfl
-    _ =  (0 - 1 : ℕ)  := rfl
-    _ =  (0 : ℕ)      := rfl
+      =  (1 - 2 : Nat)  := rfl
+    _ =  (0 - 1 : Nat)  := rfl
+    _ =  (0 : Nat)      := rfl
 
 
 
