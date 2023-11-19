16faf52cbc709bbb3e10356e8c2cc8299871df72
Mauricio Collares <mauricio@collares.org>
2023-11-19 19:22:49 +0100

    Create project skeleton

---
+++ PLFL.lean
@@ -0,0 +1,7 @@
+-- This module serves as the root of the `PLFL` library.
+-- Import modules here that should be built as part of the library.
+import PLFL.Naturals
+import PLFL.Induction
+import PLFL.Relations
+import PLFL.DeBruijn
+import PLFL.Typesig
